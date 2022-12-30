f032387f7ccc3b109492701eba10d6af81e02f3b
dagazrev <33989743+dagazrev@users.noreply.github.com>
2022-12-30 15:06:39 +0100

    comments for documentation

--- floor.py
+++ floor.py
@@ -4,14 +4,15 @@ import random
 import threading
 import pygame 
 import os
-# this class model a floor
+# === Class that models a floor and contains the necessary functions to show the floor and agroup people that make elevator calls===
+
 class Floor:
     lock = threading.Lock()
 
-    # time (in seconds) to show the people that went out from the elevator 
+    # Time (in seconds) to show the people that went out from the elevator. 
     TIME_PEOPLE_OUT = 2
 
-    # max number of random people that can be created by each request of the main
+    # Maximum number of random generated 'people' objects that can be created by each request of the main call.
     MAX_RANDOM_PEOPLE = 5
 
     EMERGENCY_POSSIBILITY = 100
@@ -36,8 +37,8 @@ class Floor:
         self.load_images()
         # self.create_people()
 
-    # create from 0 up to MAX_RANDOM_PEOPLE people ramdomly and add to the people group
-    # the people is create only if there is not people on the floor, otherwise no body is created
+    # Create from 0 up to MAX_RANDOM_PEOPLE `person` objects ramdomly and add to the `people_group`
+    # the `people_group` is create only if there is not people on the floor, otherwise no body is created.
     def create_people(self):
         self.lock.acquire()
         if len(self.people) == 0:
@@ -51,17 +52,17 @@ class Floor:
                 self.people.add_person(person)
         self.lock.release()
 
-    # function called when the elevator is called on this floor
+    # Function called when the elevator is called on this floor.
     def call_elevator(self, elevator):
         elevator.external_request(self.number)
         self.called = True
 
 
-    # open the door of the floor 
-    # people_going_out: contain the people going out from the elevator
-    # return the people that wants to enter in the elevator
-    # lock is required for the modification of the people on the floor
-    # (to no create and remove at the same tima)
+    # people_going_out: contains the people going out from the elevator.
+
+    # return the people that wants to enter in the elevator.
+
+    # A lock is required for the modification of the people on the floor to no create and remove at the same time.
     def open_door(self, people_going_out):
         self.lock.acquire()
 
@@ -76,17 +77,17 @@ class Floor:
         self.lock.release()
         return PeopleGroup(enter)
 
-    # take back the people that canno enter insithe the elevator (because is full)
+    # Take back the people that cannot enter inside the elevator due to completeness.
     def over_weight(self, people):
         self.lock.acquire()
         self.people.join(people)
         self.lock.release()
 
-    # close the door of the floor
+    # Close the door of the floor.
     def close_door(self):
         self.open = False
 
-    # remove the people that went out of the elevator
+    # Removes the subgroup of people that went out of the elevator, out of the group that remains, if any.
     def clear_people_out(self):
         if self.time_out_counter > 0:
             self.time_out_counter = self.time_out_counter - 1/self.window.FPS
@@ -106,24 +107,23 @@ class Floor:
         self.img_button_called = pygame.image.load(os.path.join('src','img', "button_called.png"))
         self.img_button_called = pygame.transform.smoothscale(self.img_button_called, self.button_size)
 
-    # draw the floor
-    # also draw the people on the floor
+    # Drawing of the floor and also drawing of the people waiting on the floor.
     def draw(self):
-        # draw door
+        # Draw door.
         if self.open:
             self.window.screen.blit(self.img_door_open, self.door_pos)
         else:
             self.window.screen.blit(self.img_door_closed, self.door_pos)
 
 
-        # draw call button
+        # Draw call button.
         if self.called:
             self.window.screen.blit(self.img_button_called, (self.door_pos[0] + self.door_size[0]-self.button_size[0], self.door_pos[1]+ self.door_size[1]/2 -self.button_size[1]/2))
         else:
             self.window.screen.blit(self.img_button, (self.door_pos[0] + self.door_size[0]-self.button_size[0], self.door_pos[1]+ self.door_size[1]/2 -self.button_size[1]/2))
 
 
-        # draw people
+        # Draw people.
         p_i = 0
         p_space = 25
         for p in self.people:
@@ -139,10 +139,12 @@ class Floor:
 
         self.clear_people_out()
 
-    # return True if the floor has people queuing for the elevator
+    # Return True if the floor has people in queue for the elevator.
     def has_people(self):
         return len(self.people) > 0
 
     def acceleration(self, mult):
         self.TIME_PEOPLE_OUT = self.TIME_PEOPLE_OUT * mult
+
+# [Go back to main.py, click here](./main.html)
 
